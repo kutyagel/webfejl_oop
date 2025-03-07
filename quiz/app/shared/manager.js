75c6a4dfab91e59f5ab5dbe1a18c3b61045f2ecf
Racz Tibor Mate <racz.tibor@bolyai.local>
2025-03-07 07:52:58 +0100

    add quiz

---
+++ quiz/app/shared/manager.js
@@ -0,0 +1,57 @@
+class Manager{
+    #array;
+
+    #currentQuestionNumber;
+
+    #selectedAnswer;
+
+    #nextQuestionCallback;
+
+    #nextAnswersCallback;
+
+    #finishCallback;
+
+    constructor(array = []){
+        this.#array = array;
+        this.#currentQuestionNumber = 0;
+        this.#selectedAnswer={};
+    }
+
+    setNextQuestionCallback(callback){
+        this.#nextQuestionCallback = callback;
+    }
+
+    setNextAnswersCallback(callback){
+        this.#nextAnswersCallback = callback;
+    }
+
+    setFinishCallback(callback){
+        this.#finishCallback = callback;
+    }
+
+    add(question){
+        this.#array.push(question);
+    }
+
+    nextQuestion(answer){
+        this.#selectedAnswer[this.#currentQuestionNumber] = answer;
+        this.#currentQuestionNumber++;
+        if(this.#currentQuestionNumber < this.#array.length){
+            this.#nextQuestionCallback(this.#array[this.#currentQuestionNumber].questionText);
+            this.#nextAnswersCallback(this.#array[this.#currentQuestionNumber].answers);
+        }else{
+            let counter = 0;
+            for(const index in this.#array){
+                if(this.#array[index].rightAnswer === this.#selectedAnswer[index]){
+                    counter++;
+                }
+            }
+            this.#finishCallback(`A kérdéssor véget ért: ${this.#array.length}/${counter} válasz volt helyes.`);
+        }
+    }
+
+    start(){
+        this.#nextQuestionCallback(this.#array[this.#currentQuestionNumber].questionText);
+        this.#nextAnswersCallback(this.#array[this.#currentQuestionNumber].answers);
+    }
+}
